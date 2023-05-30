64d43e804f26ecf44970d30feca6910c2d7929b2
Danyal <danyalkhan8271@gmail.com>
2023-05-30 20:52:47 +0530

    first commit

---
+++ process/env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB_URL:
+    "mongodb+srv://danyalkhan8271:Danyal123@cluster0.fdulbci.mongodb.net/placement-cell?retryWrites=true&w=majority",
+  SESSION_SECRET_KEY: "MongoDBisthebest",
+  PORT: 3000,
+};
